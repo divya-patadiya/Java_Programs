6a1e45562b822f613cb591e68488addb85afe691
divya-patadiya <85231705+divya-patadiya@users.noreply.github.com>
2021-06-13 15:11:43 +0530

    Add files via upload
    Java Program to reverse the string of given string with very few code of lines.

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+class ReverseString{
+	public static void main(String[] args){
+		String str="Divya";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--){
+			rev=rev+str.charAt(i);
+		}
+		System.out.println("Reverse String of "+str+" is "+ rev);
+	}
+}
